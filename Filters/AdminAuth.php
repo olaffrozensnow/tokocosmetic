933526de081a09e173b9df9e746c802313ecb5cd
olaffrozensnow <147631797+olaffrozensnow@users.noreply.github.com>
2025-12-21 11:55:13 +0700

    Add files via upload

---
+++ Filters/AdminAuth.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AdminAuth implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+
+        if (!session()->get('isLoggedIn')) {
+            return redirect()->to('/admin/login')->with('error', 'Silakan login terlebih dahulu!');
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+     
+    }
+}
